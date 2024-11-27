52544cb88ed305c0d3032feabd225f5c502257d3
sangwook16074 <qazws78941@gmail.com>
2024-11-27 21:21:58 +0900

    스웨거 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.8.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.7.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
